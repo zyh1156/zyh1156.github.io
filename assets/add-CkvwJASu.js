76aedfb9fffb56ff2793b83685116280990bb8cf
zhangyh <1156566771@qq.com>
2024-03-16 02:05:48 +0800

    reborn

--- assets/add-CkvwJASu.js
+++
@@ -1,1 +0,0 @@
-import{d as c,o as s,c as i}from"./index-CKAKOfSj.js";const m={__name:"add",setup(n){c(()=>{a()});let a=()=>{let t=new Array,o;for(let e=1;e<256;e++)o=e<10?"00"+e:e<100?"0"+e:e,t.push({index:"SCOH-"+o,name:"",img:"https://eightcha.com/scoh-"+o+"/cover.jpg",video:"https://missav.com/ja/scoh-"+o,comic:""});console.log(t)};return(t,o)=>(s(),i("div"))}};export{m as default};
